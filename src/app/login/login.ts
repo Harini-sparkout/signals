1ae542a646cd0a9167fc5b592588f896ddd2489f
Harini-sparkout <harinisri@sparkouttech.com>
2026-02-13 12:25:47 +0530

    Update login.ts

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,49 +1,41 @@
 import { Component ,signal,computed} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone:true,
-  imports: [CommonModule],
+  imports: [CommonModule,ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+form = new FormGroup ({
+        userid :new FormControl("",Validators.required) ,
+        password: new FormControl ("", [Validators.required,Validators.minLength(6)])
+})
 
-   userid = signal('');
-  password = signal('');
-useridTouched = signal(false);
-passwordTouched= signal(false);
 showPassword=signal(false);
 UseridError=computed(()=>{
- if(!this.useridTouched()) return  '' ;
- return this.userid().trim()===''? 'user id is required' : '';
+  const value = this.form.controls.userid.value ?? '';
+  if(!this.form.controls.userid.touched)return '';
+  return value.trim() === ''? 'useridrequired': '';
 });
-PasswordError=computed(()=>{
-         if(!this.passwordTouched()) return '';
-         if(this.password().trim() ===''  )return 'password is required';
-         if(this.password().length < 6 )return 'password must be atleast 6 character'; return '';
+PasswordError=computed(() =>{
+         const value = this.form.controls.password.value ?? '';
+         if(!this.form.controls.password.touched) return '';
+         if(value.trim()==='' )return 'password required';
+         if(value.length < 6) return 'min 6 characters';
+          return '';
 });
-onUseridBlur(){
-  this.useridTouched.set(true);
-}
-onPasswordBlur(){
-  this.passwordTouched.set(true);
-}
 togglePassword(){
   this.showPassword.update(v => !v);
 }
 submit(){
-  this.useridTouched.set(true);
-  this.passwordTouched.set(true);
- console.log('form submitted',{
- userid: this.userid(),
-  password:this.password()
-  });
- this.userid.set('');
- this.password.set('');
- this.useridTouched.set(false);
- this.passwordTouched.set(false);
+ this.form.markAllAsTouched();
+ if(this.UseridError() || this.PasswordError()) return;
+ console.log(this.form.value);
+ this.form.reset();
 
 }
 
