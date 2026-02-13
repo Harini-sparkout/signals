35d4cfec90427a4cf55be5ab83041ca369796136
Harini-sparkout <harinisri@sparkouttech.com>
2026-02-13 16:43:30 +0530

    Update product.ts

--- src/app/product/product.ts
+++ src/app/product/product.ts
@@ -1,4 +1,4 @@
-import { Component,linkedSignal,signal } from '@angular/core';
+import { Component,effect,linkedSignal,signal } from '@angular/core';
 
 @Component({
   selector: 'app-product',
@@ -9,8 +9,14 @@ import { Component,linkedSignal,signal } from '@angular/core';
 export class Product {
   products= signal(['laptop','mobile','tablet']);
   selectedProduct = linkedSignal(()=>this.products()[2]);
+  constructor(){
+    effect(()=>{
+      localStorage.setItem('selected',this.selectedProduct());
+      console.log('selected products:',this.selectedProduct())
+    })
+  }
   selectSecondProduct(){
-    this.selectedProduct.set(this.products()[0]);
+    this.selectedProduct.set(this.products()[1]);
   }
   newproduct(){
     this.products.set(['camera','headphones','smartwatch']);
